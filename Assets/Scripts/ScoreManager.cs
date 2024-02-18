a1f64cc5bc5a4518d984e666038de344e4260963
Chanon Rattanajaratkul <chanon.ratn@gmail.com>
2024-02-18 18:23:01 +0700

    Add score system

---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.UI;
+using TMPro;
+
+public class ScoreManager : MonoBehaviour
+{
+    public TMP_Text scoreText;
+    public int score = 0;
+
+    void Update()
+    {
+        scoreText.SetText("Score : " + score);
+    }
+}
