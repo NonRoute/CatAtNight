5d2413622230bbaede0a55779589b5a56487e85a
Chanon Rattanajaratkul <chanon.ratn@gmail.com>
2024-03-25 15:44:41 +0700

    Check cost before upgrade and decrease after upgrade

--- Assets/Scripts/ShopManager.cs
+++ Assets/Scripts/ShopManager.cs
@@ -8,13 +8,19 @@ public class ShopManager : MonoBehaviour
 {
     [SerializeField] private Player player;
     [SerializeField] private StatusUIManager statusUIManager;
+    [SerializeField] private ScoreManager scoreManager;
     [SerializeField] private Sprite FishIcon;
     [SerializeField] private Sprite MonsterIcon;
     [SerializeField] private Sprite FriendshipIcon;
     [SerializeField] private Sprite AdventureIcon;
     [SerializeField] private ShopItem health = new ShopItem();
 
-    private Sprite getIcon(ShopItem.CostType costType)
+    void Update()
+    {
+        health.button.interactable = IsUpgradable(health.currentTier, health.upgradeValue) && IsScoreSufficient(health.upgradeCostType[health.currentTier], health.upgradeCostAmount[health.currentTier]);
+    }
+
+    private Sprite GetIcon(ShopItem.CostType costType)
     {
         switch (costType)
         {
@@ -24,31 +30,66 @@ public class ShopManager : MonoBehaviour
             default: return AdventureIcon;
         }
     }
+
+    private void DecreaseScore(ShopItem.CostType costType, int value)
+    {
+        switch (costType)
+        {
+            case ShopItem.CostType.Fish:
+                scoreManager.fishScore -= value;
+                break;
+            case ShopItem.CostType.Monster:
+                scoreManager.monsterScore -= value;
+                break;
+            case ShopItem.CostType.Friendship:
+                scoreManager.friendshipScore -= value;
+                break;
+            default:
+                scoreManager.adventureScore -= value;
+                break;
+        }
+    }
+
+    private bool IsScoreSufficient(ShopItem.CostType costType, int cost)
+    {
+        switch (costType)
+        {
+            case ShopItem.CostType.Fish: return cost <= scoreManager.fishScore;
+            case ShopItem.CostType.Monster: return cost <= scoreManager.monsterScore;
+            case ShopItem.CostType.Friendship: return cost <= scoreManager.friendshipScore;
+            case ShopItem.CostType.Adventure: return cost <= scoreManager.adventureScore;
+            default: return false;
+        }
+    }
+    private bool IsUpgradable(int currentTier, List<int> upgradeValue)
+    {
+        return currentTier + 1 < upgradeValue.Count;
+    }
+
     public void UpgradeHealth()
     {
-        // TODO: Check cost
+        DecreaseScore(health.upgradeCostType[health.currentTier], health.upgradeCostAmount[health.currentTier]);
         health.currentTier++;
         player.maxHealth = health.upgradeValue[health.currentTier];
         player.health += health.upgradeValue[health.currentTier] - health.upgradeValue[health.currentTier - 1];
         statusUIManager.UpdateHealthBar(player.health, player.maxHealth);
         health.currentText.text = health.upgradeValue[health.currentTier].ToString();
-        if (health.currentTier + 1 < health.upgradeValue.Count)
+        if (IsUpgradable(health.currentTier, health.upgradeValue))
         {
 
             health.nextText.text = health.upgradeValue[health.currentTier + 1].ToString();
-            health.costSprite.sprite = getIcon(health.upgradeCostType[health.currentTier + 1]);
+            health.costSprite.sprite = GetIcon(health.upgradeCostType[health.currentTier + 1]);
             health.costText.text = health.upgradeCostAmount[health.currentTier + 1].ToString();
         }
         else
         {
-            // Last Tier
             health.nextText.text = "-";
-            health.button.interactable = false;
             health.costSprite.gameObject.SetActive(false);
             health.costText.text = "";
         }
 
     }
+
 }
 
 [System.Serializable]
