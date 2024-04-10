feba2679e680366889a53fb727c780262a48ca61
junior-meowmeow <121653436+junior-meowmeow@users.noreply.github.com>
2024-04-10 10:29:19 +0700

    Fix Fish Enable Condition

--- Assets/Scripts/Object/Fish.cs
+++ Assets/Scripts/Object/Fish.cs
@@ -1,12 +1,11 @@
 using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class Fish : MonoBehaviour
 {
     [SerializeField] private ScoreManager scoreManager;
     private bool isEnable = true;
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (!isEnable) return;
@@ -16,14 +15,15 @@ public class Fish : MonoBehaviour
             scoreManager.fishScore++;
             other.GetComponentInParent<Player>().Heal(1f);
             gameObject.GetComponent<Animator>().Play("CollectFish");
-            StartCoroutine(DistoryFish());
+            isEnable = false;
+            StartCoroutine(DestroyFish());
         }
     }
-    IEnumerator DistoryFish()
+
+    IEnumerator DestroyFish()
     {
         yield return new WaitForSeconds(0.583f);
         gameObject.SetActive(false);
-        isEnable = false;
     }
 }
 
