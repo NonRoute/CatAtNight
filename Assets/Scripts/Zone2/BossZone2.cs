0fc5b90cd67faa4941544bba482366d655cf2e01
Chanon Rattanajaratkul <chanon.ratn@gmail.com>
2024-04-21 00:44:09 +0700

    Add boss zone 2

---
+++ Assets/Scripts/Zone2/BossZone2.cs
@@ -0,0 +1,74 @@
+using Cinemachine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossZone2 : Monster
+{
+    [SerializeField] private GameObject door;
+    [SerializeField] private CinemachineVirtualCamera cam;
+    [SerializeField] public bool isStarted;
+    private Transform player;
+    private Vector2 vel;
+    public float smoothTime;
+    protected override void Start()
+    {
+        base.Start();
+        CinemachineVirtualCamera cam = FindAnyObjectByType<CinemachineVirtualCamera>();
+    }
+
+    void FixedUpdate()
+    {
+        if (isStarted)
+        {
+            if (player == null)
+            {
+                player = GameplayStateManager.Instance.Player.transform;
+                return;
+            }
+            Vector3 offset = new Vector3(0, 0, 0);
+            transform.position = Vector2.SmoothDamp(transform.position, player.position + offset, ref vel, smoothTime);
+        }
+    }
+    public void OnDead()
+    {
+        SoundManager.TryPlayNew("Boss Dead");
+        cam.Follow = GameplayStateManager.Instance.Player.GetCameraFollow();
+        DataManager.Instance.DestroyObject(gameObject);
+        gameObject.SetActive(false);
+        DataManager.Instance.DestroyObject(door);
+        door.SetActive(false);
+        GameplayStateManager.Instance.Player.SetUnlockedSkill(1);
+        DataManager.Instance.tempData.position = GameplayStateManager.Instance.Player.GetCameraFollow().position;
+        GameplayStateManager.Instance.AutoSave();
+        //Destroy(door);
+    }
+
+    public override EntityType GetEntityType()
+    {
+        return EntityType.Boss;
+    }
+
+    public override void RecieveDamage(DamageInfo damageInfo, Vector2 attackerPos)
+    {
+        health -= damageInfo.damage;
+        SoundManager.TryPlayNew("Monster Hurt");
+        if (health <= 0)
+        {
+            OnDead();
+        }
+        if (damageInfo.isInterrupt)
+        {
+            isInterrupted = true;
+            lastInterruptedTime = Time.time;
+            interruptedDuration = damageInfo.interruptDuration;
+        }
+        if (damageInfo.isBounce)
+        {
+            isBouncing = true;
+            lastBounceTime = Time.time;
+            bounceDuration = damageInfo.bounceDuration;
+            bounceVelocity = damageInfo.bounceSpeed * ((Vector2)transform.position - attackerPos).normalized;
+        }
+    }
+}
