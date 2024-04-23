7e14a81ffa143cf26d112572cac336fdd893aa4a
junior-meowmeow <121653436+junior-meowmeow@users.noreply.github.com>
2024-04-23 15:29:17 +0700

    Finish Zone 3 Tiles

---
+++ Assets/Scripts/Zone3/Zone3Manager.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Zone3Manager : MonoBehaviour
+{
+    void Start()
+    {
+        SoundManager.TryPlayMusic("Zone 3 Music");
+    }
+}
